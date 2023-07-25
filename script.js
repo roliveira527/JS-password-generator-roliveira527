c411663bc8daa069f9c225f94f48fc5e8ad9cdaf
Ricardo Oliveira <r.deoliveira527@gmail.com>
2023-07-25 19:06:38 +0100

    implemented js functions for password generation

--- script.js
+++ script.js
@@ -90,17 +90,74 @@ var upperCasedCharacters = [
 
 // Function to prompt user for password options
 function getPasswordOptions() {
+  var length = parseInt(prompt("Enter the length of the password (8-128 characters):"));
 
+  // Validate the input for length
+  if (isNaN(length) || length < 8 || length > 128) {
+    alert("Password length must be between 8 and 128 characters.");
+    return;
+  }
+
+  var includeLowercase = confirm("Include lowercase characters?");
+  var includeUppercase = confirm("Include uppercase characters?");
+  var includeNumeric = confirm("Include numeric characters?");
+  var includeSpecial = confirm("Include special characters?");
+
+  // Validate that at least one character type is selected
+  if (!includeLowercase && !includeUppercase && !includeNumeric && !includeSpecial) {
+    alert("You must select at least one character type.");
+    return;
+  }
+
+  return {
+    length: length,
+    includeLowercase: includeLowercase,
+    includeUppercase: includeUppercase,
+    includeNumeric: includeNumeric,
+    includeSpecial: includeSpecial,
+  };
 }
 
 // Function for getting a random element from an array
 function getRandom(arr) {
-
+  var randomIndex = Math.floor(Math.random() * arr.length);
+  return arr[randomIndex];
 }
 
 // Function to generate password with user input
 function generatePassword() {
+  var options = getPasswordOptions();
+  if (!options) return; // If options are not provided or invalid, return null.
 
+  var allCharacters = [];
+  var generatedPassword = "";
+
+  if (options.includeLowercase) {
+    allCharacters = allCharacters.concat(lowerCasedCharacters);
+    generatedPassword += getRandom(lowerCasedCharacters);
+  }
+
+  if (options.includeUppercase) {
+    allCharacters = allCharacters.concat(upperCasedCharacters);
+    generatedPassword += getRandom(upperCasedCharacters);
+  }
+
+  if (options.includeNumeric) {
+    allCharacters = allCharacters.concat(numericCharacters);
+    generatedPassword += getRandom(numericCharacters);
+  }
+
+  if (options.includeSpecial) {
+    allCharacters = allCharacters.concat(specialCharacters);
+    generatedPassword += getRandom(specialCharacters);
+  }
+
+  // Fill in the rest of the password
+  for (var i = generatedPassword.length; i < options.length; i++) {
+    generatedPassword += getRandom(allCharacters);
+  }
+
+  return generatedPassword;
 }
 
 // Get references to the #generate element
